cabd9e9984d8cc6fc25d58be0fa64a7a6578c387
FireyJS <57120755+fireyjs@users.noreply.github.com>
2021-12-18 13:52:07 -0500

    Update welcome.bat

--- welcome.bat
+++ welcome.bat
@@ -1,26 +1,14 @@
 @echo off
 echo Welcome to TermCord!
-echo Just put in a token, and you can talk and send messages as bots!
-timeout 3 >nul
+pause
 cls
-echo.
 echo Note: This is a client made by FireyJS and should not be used for illegal activity. If you see someone with this client doing illegal stuff, then report them to TOS, or just block the bot.
 echo.
-timeout 4 >nul
+timeout 2 >nul
 cls
 echo.
-echo =========================
-echo    TermCord - Login
-echo  =========================
-set /p token="Token: "
-cls
-echo.
-echo =========================
-echo         TermCord
-echo =========================
 echo  Connecting...
 goto Client
 
 :Client
-node client.js --token=%token%
-goto Client
+node client.js
